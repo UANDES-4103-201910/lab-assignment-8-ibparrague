f4490cb86a33dffd41f2685aed83002b9c9520cb
IgnacioParrague <ibparrague@miuandes.cl>
2019-05-08 14:09:30 -0400

    done

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,57 @@
-// Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+$(document).ready(function(){
+
+class KeyboardController {
+    constructor(id_target) {
+        this.target= $(id_target);
+        this.element = document.getElementById("targetelement");
+    }
+
+    toggleKeyboard(){
+      var k = document.getElementById("kcontainer")
+      if (k.style.display == "none"){
+        k.style.display = "block"
+      }
+      else{
+        k.style.display = "none"
+      }
+    }
+
+    keyPressed(key) {
+
+        if (key.text().includes("Tab")){
+          this.element.innerHTML += "tab_space";
+        }
+        else if (key.text().includes("Accept")){
+          alert("Accepted")
+        }
+        else if (key.text().includes("Cancel")){
+          alert("Canceled")
+        }
+        else if (key.text().includes("Enter")){
+          this.element.innerHTML += "<p>"
+        }
+        else if (key.text().includes("Shift")){
+          this.element.innerHTML += "s_"
+        }
+        else if (key.text().includes("Bksp")){
+          this.element.innerHTML = this.element.innerHTML.slice(0,-1)
+        }
+        else{
+          this.element.innerHTML += key.text()
+        }
+    }
+}
+
+
+var x = new KeyboardController("targetelement")
+
+$(".key").click(function(){
+  x.keyPressed($(this))
+});
+
+$("#asd").click(function(){
+  x.toggleKeyboard()
+});
+
+
 });
